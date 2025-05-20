b7755ca62b3d8968d029ee1f1c8efa773c6786d9
Alexander Biryukov <abirukov2014@yandex.ru>
2025-05-20 23:30:52 +0500

    рефакторинг useQuery Создал для категорий и операций отдельные хули

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -7,7 +7,7 @@ const List = ({ defaultItems, type = 'dashboard' }: ListProps) => {
   return (
     <article className={styles.list}>
       <div className={styles.wrapper}>
-        {defaultItems.map((item: any) => (
+        {!!defaultItems && defaultItems.map((item: any) => (
           <Link to={`/${type}/${item.id}`} className={styles.item} key={item.id}>
             <Informer {...item} />
           </Link>
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,27 +1,19 @@
 import type { FC } from 'react';
-import { useQuery } from '@apollo/client';
-import { OperationAddForm, Modal, List } from '@/components';
-import { OPERATIONS } from '@/graphql';
-import { useModal } from '@/hooks';
+import { List, Modal, OperationAddForm } from '@/components';
+import { useDashboard, useModal } from '@/hooks';
 import { Button } from '@/ui';
 
 const Dashboard: FC = () => {
   const modal = useModal();
-  const { data, error, loading } = useQuery(OPERATIONS, {
-    variables: {
-      input: {},
-    },
-    fetchPolicy: 'network-only',
-  });
+  const { error, loading, operations } = useDashboard()
 
   if (loading) return <p>Загрузка...</p>;
   if (error) return <p>Ошибка: {error.message}</p>;
-  const operations = data?.operations?.getMany?.data || [];
 
   return (
     <div>
       <div style={{ display: 'flex', justifyContent: 'center', margin: '24px' }}>
-        <Button onClick={modal.show} text="Добавить операцию" />
+        <Button text="Добавить операцию" onClick={modal.show} />
       </div>
       <List defaultItems={operations} type="dashboard" />
       <Modal visible={modal.visible} onClose={modal.hide}>
