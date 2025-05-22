fbcca3dcd13c9e749dfe520aa307e73e7645b7a9
Будылин Константин Владимирович <konstantin.budylin@domrf.ru>
2025-05-22 11:16:33 +0200

    обновлен компонент BackButton

--- src/components/back-button/BackButton.tsx
+++ src/components/back-button/BackButton.tsx
@@ -1,42 +1,34 @@
 import { useEffect, useRef } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { useAuth } from '@/stores/useAuth';
 import styles from './BackButton.module.css';
 
 const BackButton = () => {
+  const { isAuth } = useAuth();
   const navigate = useNavigate();
   const location = useLocation();
-  const { isAuth } = useAuth();
-
-  const ALLOWED_PATHS = ['/dashboard', '/profile', '/categories'];
-
-  const historyRef = useRef<string[]>([]);
+  const prevPathRef = useRef<string | null>(null);
 
   useEffect(() => {
-    if (isAuth && ALLOWED_PATHS.includes(location.pathname)) {
-      if (historyRef.current[historyRef.current.length - 1] !== location.pathname) {
-        historyRef.current = [...historyRef.current, location.pathname];
-      }
-    }
-  }, [location.pathname, isAuth]);
+    prevPathRef.current = location.pathname;
+  }, [location]);
 
-  const END_ARRAY_ITEM = 2;
+  if (!isAuth) {
+    return null;
+  }
 
   const handleGoBack = () => {
-    const history = historyRef.current;
-    if (history.length > 1) {
-      const prevPath = history[history.length - END_ARRAY_ITEM];
-      navigate(prevPath);
-    } else {
-      navigate('/dashboard');
-    }
+    navigate(-1);
   };
 
-  const shouldShowButton =
-    isAuth && ALLOWED_PATHS.includes(location.pathname) && historyRef.current.length !== 0;
+  if (prevPathRef.current === '/login' || prevPathRef.current === '/registration') {
+    return null;
+  }
 
-  if (!shouldShowButton) return null;
+  if (location.pathname === '/') {
+    return null;
+  }
 
   return (
     <button className={styles.button} type="button" onClick={handleGoBack}>
